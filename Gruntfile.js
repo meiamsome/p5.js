2505a589b2929b3495be616d88d22b38e67b5513
Spongman <piersh@hotmail.com>
2018-05-04 23:43:59 -0700

    more package.json cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -457,9 +457,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-http');
+  //grunt.loadNpmTasks('grunt-http');
   grunt.loadNpmTasks('grunt-minjson');
-  grunt.loadNpmTasks('grunt-mocha');
+  //grunt.loadNpmTasks('grunt-mocha');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-newer');
   grunt.loadNpmTasks('grunt-release-it');
@@ -490,7 +490,7 @@ module.exports = function(grunt) {
     'build',
     'connect',
     'mochaChrome',
-    'mocha',
+    //'mocha',
     'mochaTest'
   ]);
   grunt.registerTask('test:nobuild', ['eslint:test', 'connect', 'mocha']);
